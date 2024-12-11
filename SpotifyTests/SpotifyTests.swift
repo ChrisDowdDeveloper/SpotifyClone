8e0504057c85d1ddbd5a260437dd7e93c7690c38
Chris Dowd <chrisdowd1996@gmail.com>
2024-12-11 15:43:49 -0600

    Initial Commit

---
+++ SpotifyTests/SpotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyTests.swift
+//  SpotifyTests
+//
+//  Created by Christopher Dowd on 12/11/24.
+//
+
+import Testing
+@testable import Spotify
+
+struct SpotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
